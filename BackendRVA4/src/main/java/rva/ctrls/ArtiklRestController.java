5ebf1fd174542f2e284125ba2eb2198f6c19336c
vukmanovicmilos <vukmanovic.milos@gmail.com>
2019-05-15 10:49:00 +0200

    crossorigin

--- BackendRVA4/src/main/java/rva/ctrls/ArtiklRestController.java
+++ BackendRVA4/src/main/java/rva/ctrls/ArtiklRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import io.swagger.annotations.ApiOperation;
 import rva.jpa.Artikl;
 import rva.reps.ArtiklRepository;
 
+@CrossOrigin
 @Api(tags = {"Artikl CRUD operacije"})
 @RestController
 public class ArtiklRestController {
